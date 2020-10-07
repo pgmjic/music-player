5ded001a835feb05d4653ff0de20963afa83e797
John Chong <johnichong@gmail.com>
2020-10-07 09:07:01 -0400

    Added the changes for the Progress Bar.

---
+++ src/components/Progress.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Progress extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render(){
+    return(
+      <div className="myprogress has-text-light" >
+        <span>{this.props.elapsed}</span>
+        <progress className="progress is-small is-danger " 
+           value={this.props.position}
+           max="1"></progress>
+        <span>{this.props.total}</span>
+      </div>
+    )
+  }
+
+}
+
+export default Progress
